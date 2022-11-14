9a7d7f2c5ba9ddaa9cd91d3d1ddb101f7b4d6964
thotenn <josenriqueee@gmail.com>
2022-11-14 09:48:26 -0300

    .

--- app/posts/[id]/ListOfPosts.jsx
+++ app/posts/[id]/ListOfPosts.jsx
@@ -1,8 +1,17 @@
+import Link from "next/link";
 import { LikeButton } from "./LikeButton";
 
 const fetchPosts = () => {
-    return fetch('https://jsonplaceholder.typicode.com/posts')
+    return fetch('https://jsonplaceholder.typicode.com/posts', {
+        next: {  // Lo que hace esto es que hace un fetch cada 60 segundos o los que quieras, y actualiza los html
+            revalidate: 3
+        }
+    })
         .then(res => res.json());
+    /*return fetch('https://jsonplaceholder.typicode.com/posts', { 
+        cache: 'no-store' // Esto hace que al generar el html este fetch lo haga cada vez que se solicite el archivo y no solo una unica vez al generar el html
+    })
+    .then(res => res.json());*/
 
 }
 
@@ -11,9 +20,11 @@ export async function ListOfPosts() {
 
     return posts.slice(0, 5).map(post => (
         <article key={post.id}>
-            <h2 style={{ color: "#09f" }}>{post.title}</h2>
-            <p>{post.body}</p>
-            <LikeButton id={post.id}/>
+            <Link href={`/posts/${post.id}`}>
+                <h2 style={{ color: "#09f" }}>{post.title}</h2>
+                <p>{post.body}</p>
+                <LikeButton id={post.id} />
+            </Link>
         </article>
     ))
 }
---
+++ app/posts/layout.jsx
@@ -0,0 +1,8 @@
+export default function PostsLayout ({ children }) {
+    return (
+        <div>
+            <h1>Este es el layaout de los POsts 0</h1>
+            {children}
+        </div>
+    )
+}
